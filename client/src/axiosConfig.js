26acf82069908bcd6fe7fba2a84935ef5de3de32
Rehan <callmedt2005@gmail.com>
2024-05-17 16:45:46 +0600

    ajaira jinish bad new project lets go

--- client/src/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-// src/axiosConfig.js
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:5000", // Replace with your backend server URL
-});
-
-export default instance;
